330bc294b5de5d30c3cc24312a1d6170337ccd4b
Ankitkr2506 <ak9852768165@gmail.com>
2025-06-09 01:18:02 +0530

    api done

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL); // ← missing await
+    console.log("✅ Database connected");
+  } catch (error) {
+    console.error("❌ DB connection failed:", error);
+    throw error;
+  }
+};
+
+export default connectDb;
---
+++ backend/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDb from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173", // ✅ Set your frontend origin here
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 5000;
+
+// Routes
+app.use("/api/auth", authRouter);
+app.get("/", (req, res) => {
+  res.send("✅ Backend is working!");
+});
+
+// Connect DB and start server
+connectDb()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`✅ Server started on http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ Database connection failed", err);
+  });
