bef861bbaae0fd26d8465e4f97817a3a98e1b350
Ankitkr2506 <ak9852768165@gmail.com>
2025-06-27 23:16:21 +0530

    fixing error

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import React, { createContext, useEffect, useState } from 'react';
 export const userDataContext = createContext();
 
 const UserContext = ({ children }) => {
-  const serverUrl = "http://localhost:8000"
+  const serverUrl = "https://ai-virtual-assistant-gilt.vercel.app"
   const [userData, setUserData] = useState(null)
   const [frontendImage, setfrontendImage]=useState(null)
     const [BackendImage, setBackendImage]=useState(null)
